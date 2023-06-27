291186c63040f0c033c3d7f23d93592413f4f1c8
Hoai Nhan <hoainhan.lhp@gmail.com>
2023-06-27 10:54:24 +0700

    CORS

--- routes/dishRouter.js
+++ routes/dishRouter.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const authenticate = require("../auth")
+const cors = require('../cors')
 
 const dishRouter = express.Router();
 const dishesController = require('../controller/dishesController')
@@ -8,24 +9,28 @@ dishRouter.use(express.json());
 
 dishRouter
   .route("/")
-  .get(dishesController.findAll)
-  .post(authenticate.verifyUser, authenticate.verifyAdmin, dishesController.create)
-  .delete(authenticate.verifyUser, authenticate.verifyAdmin, dishesController.deleteAll)
+  .options(cors.corsWithOptions, (req, res) => { res.status(200).json('cors') })
+  .get(cors.cors, dishesController.findAll)
+  .post(cors.corsWithOptions, authenticate.verifyUser, authenticate.verifyAdmin, dishesController.create)
+  .delete(cors.corsWithOptions, authenticate.verifyUser, authenticate.verifyAdmin, dishesController.deleteAll)
 
 dishRouter
   .route('/:dishId')
-  .get(dishesController.getById)
-  .put(authenticate.verifyUser, authenticate.verifyAdmin, dishesController.updateById)
-  .delete(authenticate.verifyUser, authenticate.verifyAdmin, dishesController.deleteById)
+  .options(cors.corsWithOptions, (req, res) => { res.status(200).json('cors') })
+  .get(cors.cors, dishesController.getById)
+  .put(cors.corsWithOptions, authenticate.verifyUser, authenticate.verifyAdmin, dishesController.updateById)
+  .delete(cors.corsWithOptions, authenticate.verifyUser, authenticate.verifyAdmin, dishesController.deleteById)
 
 dishRouter
   .route('/:dishId/comments')
-  .get(dishesController.getComments)
+  .options(cors.corsWithOptions, (req, res) => { res.status(200).json('cors') })
+  .get(cors.cors, dishesController.getComments)
 
 dishRouter
   .route('/:dishId/comments/:commentId')
-  .get(dishesController.getCommentById)
-  .put(authenticate.verifyUser, dishesController.updateCommentById)
-  .delete(authenticate.verifyUser, dishesController.deleteCommentById)
+  .options(cors.corsWithOptions, (req, res) => { res.status(200).json('cors') })
+  .get(cors.cors, dishesController.getCommentById)
+  .put(cors.corsWithOptions, authenticate.verifyUser, dishesController.updateCommentById)
+  .delete(cors.corsWithOptions, authenticate.verifyUser, dishesController.deleteCommentById)
 
 module.exports = dishRouter;
